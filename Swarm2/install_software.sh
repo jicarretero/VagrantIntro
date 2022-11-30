46fa8cc53e240a8a9e41d2c2cb94fbfad3086197
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2022-11-30 16:36:31 +0100

    Cambios instalando docker swarm. Usando bucles

--- Swarm2/install_software.sh
+++ Swarm2/install_software.sh
@@ -3,9 +3,6 @@
 ## 
 ## Install Docker
 ##
-add-apt-repository -y ppa:gluster/glusterfs-7
-apt-get -y update 
-
 DEBIAN_FRONTEND='noninteractive' apt-get dist-upgrade  -o Dpkg::Options::="--force-confdef" -o Dpkg::Options="--force-confold" -y
 
  apt-get install -y \
@@ -15,22 +12,40 @@ DEBIAN_FRONTEND='noninteractive' apt-get dist-upgrade  -o Dpkg::Options::="--for
    curl \
    lsof \
    vim \
-   docker \
-   docker.io \
    glusterfs-client \
    glusterfs-server
 
 apt -y clean
 apt -y autoremove
 
+## Instalar Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt-cache policy docker-ce
+sudo apt-get --quiet install -y docker-ce
+
+
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+
+usermod -aG docker vagrant
+
+curl -L "https://github.com/docker/compose/releases/download/v2.11.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+
+mkdir -p ~vagrant/.docker/cli-plugins
+ln -s /usr/local/bin/docker-compose ~vagrant/.docker/cli-plugins/docker-compose
+chown -R vagrant.vagrant ~vagrant/.docker
+
+mkdir -p ~root/.docker/cli-plugins
+ln -s /usr/local/bin/docker-compose ~root/.docker/cli-plugins/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
 systemctl enable docker
 systemctl start docker
-
-## 
-## Install Docker-compose
-##
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
 
 # Format and mount drive
 if [ -e /dev/sdc ]; then
